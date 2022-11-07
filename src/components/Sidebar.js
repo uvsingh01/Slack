6ee912e235047b727c65c9bbc4d648d6944243af
uvsingh01 <mr.uvsingh@gmail.com>
2022-11-07 11:12:43 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -45,7 +45,7 @@ export default Sidebar;
 
 const SidebarContainer = styled.div`
 background-color: var(--slack-color);
-flex:0.3;
+flex:0.35;
 border-top: 1px solid #49274b;
 color: white;
 margin-top: 60px;
